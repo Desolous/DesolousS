3dc5cd09a5162f699eac7791bc1513fa1986ecae
Desolous <52091781+Desolous@users.noreply.github.com>
2019-07-04 18:18:43 +0300

    Update DJ_Desolous.py

--- DJ_Desolous.py
+++ DJ_Desolous.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
-from discord.ext.commands import has_permissions, CheckFailure
 
 bot = discord.Client()
 bot_prefix="*"
@@ -15,24 +14,19 @@ async def on_ready():
     print ("Aktif")
 
 @bot.command(pass_context=True)
-async def nelanbu(ctx):
-    await ctx.send(''' Hey Yo {0} ! \n"*sg" İtediğin kişiyi kickle !  (Sadece Yönetici) \n"*sgoç" İstediğin kişiyi banla ! (Sadece Yönetici) \n"*sat" İstediğin kişinin sattığını belirt ! (Herkes)'''.format(ctx.author.mention))
-
-@bot.command(pass_context=True)
 async def sat(ctx, member:discord.Member):
     await ctx.send(member.mention + " SATIYOR !!!!")
 
-@bot.command(pass_context = True)
-@has_permissions(manage_roles=True, ban_members=True, kick_members=True)
-async def sg(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(member.mention + " Yine bekleriz :) Ama önce adam ol.")
+@bot.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name="boşlar")
+    await channel.send(member.mention + " Hoşgeldin Paşam !")
 
-@bot.command(pass_context = True)
-@has_permissions(manage_roles=True, ban_members=True)
-async def sgoç(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(member.mention + " Yolun açık olsun paşam :)")
 
-bot.run("NTk2MDAyNzA3MDYwMjkzNjQz.XRzPag.zzxHWVGEouOKEF93tnv2nZbVI1o") 
+@bot.event
+async def on_member_remove(member):
+    channel = discord.utils.get(member.guild.channels, name="boşlar")
+    await channel.send(member.mention + " Hop Hemşerim Nereye ?")
+
+bot.run("NTk2MDAyNzA3MDYwMjkzNjQz.XR4Yyg.c3M8Q-Uvc6X0TGKI_IZaM0k5wFE") 
 
